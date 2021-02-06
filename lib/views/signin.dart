33486d63c21f4498e4fe19a4510f0850dba4859f
wsacha <43878011+wsacha@users.noreply.github.com>
2021-02-06 23:51:52 +0100

    creates sign in screen

---
+++ lib/views/signin.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/widgets/widget.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: appBarScreens(context, 'Sign in'),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height - 50,
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration('email'),
+                  ),
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration('password'),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text(
+                        'Forgot Password?',
+                        style: simpleTextStyle(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color(0xff007EF4),
+                          const Color(0xff2A75BC)
+                        ]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      'Sign in',
+                      style: mediumTextStyle(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      'Sign in with Google',
+                      style: TextStyle(color: Colors.black87, fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have account? ",
+                        style: mediumTextStyle(),
+                      ),
+                      Text(
+                        'Register now',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            decoration: TextDecoration.underline),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Widget appBarScreens(BuildContext context, String screenTitle) {
+  return AppBar(
+    title: Text('$screenTitle'),
+  );
+}
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.white54),
+      focusedBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+      enabledBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(color: Colors.white, fontSize: 16);
+}
+
+TextStyle mediumTextStyle() {
+  return TextStyle(color: Colors.white, fontSize: 17);
+}
