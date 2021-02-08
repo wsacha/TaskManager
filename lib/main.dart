e2698ed4f90aef4d4f2e4edaae36502ad2ac12d9
wsacha <43878011+wsacha@users.noreply.github.com>
2021-02-08 19:20:00 +0100

    Added Firebase, Sign up is working, Added validation to entered data Sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/views/signin.dart';
 import 'package:task_manager/views/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
---
+++ lib/views/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeUser extends StatefulWidget {
+  HomeUser({Key key}) : super(key: key);
+
+  @override
+  _HomeUserState createState() => _HomeUserState();
+}
+
+class _HomeUserState extends State<HomeUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
