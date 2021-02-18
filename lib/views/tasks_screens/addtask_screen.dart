a15a1f8984f0d2b6f83fd103e80ccfb87097b728
wsacha <43878011+wsacha@users.noreply.github.com>
2021-02-19 00:50:22 +0100

    Added TimePicker to addtask screen

--- lib/views/tasks_screens/addtask_screen.dart
+++ lib/views/tasks_screens/addtask_screen.dart
@@ -19,11 +19,14 @@ class _AddTaskState extends State<AddTask> {
   final formKey = GlobalKey<FormState>();
   DatabaseMethods databaseMethods = new DatabaseMethods();
   DateTime _dateTime;
+  TimeOfDay _time = TimeOfDay.now();
   TextEditingController taskTitleTextEditingController = new TextEditingController();
   TextEditingController descriptionTextEditingController = new TextEditingController();
 
   _addTask(String userName, String roomId) {
     if (formKey.currentState.validate() && _dateTime != null) {
+      _dateTime =
+          new DateTime(_dateTime.year, _dateTime.month, _dateTime.day, _time.hour, _time.minute);
       Task task = new Task(
           id: DateTime.now().millisecondsSinceEpoch.remainder(10000000).toString(),
           roomId: roomId,
@@ -126,6 +129,54 @@ class _AddTaskState extends State<AddTask> {
                                     _dateTime = date;
                                   });
                                 });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "Due time: ",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              _time == null ? "low" : _time.format(context).toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: RaisedButton.icon(
+                              shape:
+                                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                              color: Color(0xff145C9E),
+                              label: Text(
+                                "Time",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              icon: Icon(
+                                Icons.access_time,
+                                color: Colors.white,
+                              ),
+                              onPressed: () async {
+                                TimeOfDay t = await showTimePicker(
+                                    context: context, initialTime: TimeOfDay.now());
+                                if (t != null) {
+                                  setState(() {
+                                    _time = t;
+                                  });
+                                }
                               },
                             ),
                           ),
